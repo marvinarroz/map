1d8384c0124e401e848935c8ed117ac5ecd911c8
Marvin Arroz <marvinarroz@gmail.com>
2015-03-06 21:45:01 -0800

    Map markers works with filters now. Added in bootstrap css

--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,61 @@
 var mapApp = angular.module('mapApp',["leaflet-directive"]); 
 
-mapApp.controller('MapController', [ '$scope' , function($scope) {
-/**	$scope.years = [
-		{'name' : 2000},
-		{'name' : 2001}
-	];*/
+mapApp.filter('filterMarkers', function() {
+	return function(data, year, gender, cause, age, category) {
+		var filtered = [];
+		for (var i=0; i<data.length; i++) {
+			if ((year === undefined || year === null || data[i].properties.year == year) && 
+			     (gender === undefined || gender === null || data[i].properties.gender == gender) &&
+		   	     (cause === undefined || cause === null || data[i].properties.cause == cause) &&
+			     (age === undefined || age === null || data[i].properties.age == age) &&
+			     (category === undefined || category === null || data[i].properties.category == category)) {
+				filtered.push(data[i]);
+			}
+		}
+		return filtered;
+	};
+});
+
+mapApp.controller('MapController', [ '$scope' , '$filter' , function($scope, $filter) {
+	$scope.years = [];
+	for (var i=2000;i<=2015;i++){
+		$scope.years.push(i);
+	}
+	$scope.genders = [
+		'Male','Female'
+	];
+	$scope.causes = [
+		'Asphyxiation', 'Blunt Force', 'Shooting', 'Stabbing', 'Tasering', 'Unknown', 'Vehicle'
+	];
+	$scope.ages = [];
+	for (var i=0;i<=90;i++){
+		$scope.ages.push(i);
+	}
+	$scope.categories = ['Accident', 'Homicide', 'Justifiable Homicide', 'Negligent Homicide', 'Officer-Involved Homicide', 'Unincorporated Homicide'];
 
 	angular.extend($scope, {
 		vallejo: {
 			lat: 38.1139,
-			lng: 138.86,
-			zoom: 4
+			lng: -122.2241,
+			zoom: 13
 		},
 		defaults: {
 			tileLayer: 'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
-			scrollWheelZoom: true
+			scrollWheelZoom: false 
 		}
 	});
-	/**
-	$scope.$watchCollection("yearSelect",
-		function(newVal, oldVal){
-			console.log(newVal);
-			console.log(oldVal);
-		});
-	angular.extend($scope, {
+	
+		angular.extend($scope, {
 		geojson: {
 			data: data
 		}
-	});*/
+	});
+	$scope.$watchGroup(['yearSelect', 'genderSelect', 'causeSelect', 'ageSelect', 'categorySelect'], 
+		function(newValues, oldValues, scope) {
+			var filtered = $filter('filterMarkers')(data,$scope.yearSelect,$scope.genderSelect,$scope.causeSelect,$scope.ageSelect,$scope.categorySelect);
+			$scope.geojson = {
+				data:filtered
+			};
+		});
 }]);
 
